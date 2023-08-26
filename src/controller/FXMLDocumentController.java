639218c63432aeaa776c439112168a872c05b83f
RaeIz <140860784+RaeIz@users.noreply.github.com>
2023-08-25 19:58:41 -0500

    Subida

--- src/controller/FXMLDocumentController.java
+++ src/controller/FXMLDocumentController.java
@@ -45,6 +45,19 @@ public class FXMLDocumentController implements Initializable {
     private Button finishSetup;
     @FXML
     private TextArea textAreaTXT;
+    
+    public void crearCarros(ActionEvent event){
+    // TODO code application logic here        
+//           
+//    ArrayList<receptor> losReceptores = new ArrayList<>();        
+//    //Loop            
+//    //formula para generar valores aleatorios con un maximo y minimo 
+//        int         
+//        int b = (int)(Math.random()*(max-min+1)+min);       
+//        int elModelo = (int) (Math.random() * (2024 - 2000 + 1) + 2000);        //por ahora el nombre estara en blanco para confirmar que el loop funciona         
+//        String elNombre = "David";        int elTiempoLan = (int) (Math.random() * (5 - 1 + 1) + 1);        
+//        colaCarros.encolar(new Carro(elModelo, elNombre, elTiempoLan));
+    }
 
     @FXML
     private void startSetup(ActionEvent event) {
@@ -58,7 +71,8 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-
+        
+        Cola<Carro> colaCarros = new Cola<>();
     }
 
 }
